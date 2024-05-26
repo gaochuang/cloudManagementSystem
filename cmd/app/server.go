b417aa1c92c09139bfc91c1dfeb1d067ce20abb7
gaochuang <ienjoyarmlinux@163.com>
2024-05-27 06:54:04 +0800

    Reflect code

--- cmd/app/server.go
+++ cmd/app/server.go
@@ -2,24 +2,50 @@
 package app
 
 import (
+	"fmt"
+	"github.com/gaochuang/cloudManagementSystem/cmd/app/options"
+	"github.com/gaochuang/cloudManagementSystem/pkg/log"
 	"os"
 
 	"github.com/spf13/cobra"
 )
 
-// rootCmd represents the base command when called without any subcommands
-var rootCmd = &cobra.Command{
-	Use:   "managerServer",
-	Short: "cloud management system",
-	Long:  `welcome to https://github.com/gaochuang/cloudManagementSystem`,
-	Run: func(cmd *cobra.Command, args []string) {
+func NewServerCommand() *cobra.Command {
+	opt := options.NewOptions()
+	if nil == opt {
+		log.Logger.LogError("create options failed")
+	}
+	cmd := &cobra.Command{
+		Use:   "managerServer",
+		Short: "cloud management system",
+		Long:  `welcome to https://github.com/gaochuang/cloudManagementSystem`,
+		Run: func(cmd *cobra.Command, args []string) {
+			if err := opt.Initialize(); err != nil {
+				fmt.Fprintf(os.Stderr, "%v\n", err)
+				os.Exit(1)
+			}
+			if err := run(opt); err != nil {
+				fmt.Fprintf(os.Stderr, "%v\n", err)
+				os.Exit(1)
+			}
+		},
 
-	},
+		Args: func(cmd *cobra.Command, args []string) error {
+			for _, arg := range args {
+				if len(arg) > 0 {
+					return fmt.Errorf("%q does not take any arguments, got %q", cmd.CommandPath(), args)
+				}
+			}
+			return nil
+		},
+	}
+	opt.BindConfigurationFlag(cmd)
+	return cmd
 }
 
-func Execute() {
-	err := rootCmd.Execute()
-	if err != nil {
-		os.Exit(1)
+func run(opt *options.Options) error {
+	if err := opt.RunHttpServer(); err != nil {
+		return err
 	}
+	return nil
 }
